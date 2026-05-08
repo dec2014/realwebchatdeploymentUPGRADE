624cf04946ab5d4966f6646c8aae56a4fd50a33c
Vishesh Jain <visheshjain@Visheshs-MacBook-Air.local>
2026-05-08 17:25:44 +0530

    fixed csrf trusted origins

--- jatte/settings.py
+++ jatte/settings.py
@@ -146,3 +146,7 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://realwebchatdeploymentupgrade.onrender.com",
+]
